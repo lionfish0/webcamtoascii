becd038a6cffe50918965a2998cc711a2655b9fa
Michael Smith <m.t.smith@sheffield.ac.uk>
2023-04-06 16:29:05 +0100

    increased 'brightness' and res'

--- webcamtoascii/__init__.py
+++ webcamtoascii/__init__.py
@@ -4,8 +4,11 @@ import numpy as np
 def ascii_draw(mat):
     symbols = np.array([s for s in ' .,:-=+*X#@@'])#[::-1]
     msg = ""
-    mat = (11*mat/(np.max(mat)+1)).astype(int)
+    maxnum = np.percentile(mat,80)
+    #maxnum = np.max(mat)+1
+    mat = (11*mat/(maxnum)).astype(int)
     mat[mat<0] = 0
+    mat[mat>11] = 11
     for i in range(0,len(mat),2):
         msg+=''.join(symbols[mat][i])+"\n"
     return msg
@@ -16,7 +19,11 @@ def run():
     while True:
         check, frame = cam.read()
         if frame is None: continue
-        resized_frame = cv2.resize(np.mean(frame,2), (140,90)) #(91,53))
+        #resized_frame = cv2.resize(np.mean(frame,2), (140,90)) #(91,53))
+        #resized_frame = cv2.resize(np.mean(frame,2), (182,117)) #(91,53))
+        
+        resized_frame = cv2.resize(np.mean(frame,2), (210,135)) #(91,53))
+
         #print(ascii_draw(np.mean(frame[::9,::7],2)))
         print(ascii_draw(resized_frame))
 
